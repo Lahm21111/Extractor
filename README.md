98c22efaf3bdcc544212e4e1358c104ecef8bfb0
Ziang Liu <ziangliu@tudelft.nl>
2024-09-27 15:40:54 +0200

    add the time synchronizer to the node, just to make sure the image and audio are from the same time

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This node subscribes to image and audio data, merging messages with similar time
 ## Build
 To build the project, use the following command. This avoids potential dependency errors:
 
-colcon build --merge-install --symlink-install --cmake-args -Wall -Wextra -Wpedantic
+`colcon build --merge-install --symlink-install --cmake-args -Wall -Wextra -Wpedantic`
 
 ## Run
 run the extractor with `ros2 run extractor_node extract`
--- extractor_node/src/extractor_with_time_synchronizer.cpp
+++ extractor_node/src/extractor_with_time_synchronizer.cpp
@@ -17,22 +17,13 @@ public:
     rclcpp::QoS image_qos(rclcpp::KeepLast(10));
     image_qos.best_effort();
 
-
     // // subscribe to the image topic 
-    // image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-    //   "/cae_micarray/images/pict", image_qos, std::bind(&ExtractorNode::image_callback, this, std::placeholders::_1));
+    //  auto image_subscriber = this->create_subscription<sensor_msgs::msg::Image>(
+    //         "/cae_micarray/images/pict", image_qos,
+    //         [this](const sensor_msgs::msg::Image::SharedPtr msg) {
+    //         });
 
-    // // subscribe to the audio topic
-    // audio_subscription_ = this->create_subscription<cae_microphone_array::msg::AudioStream>(
-    //   "/cae_micarray/audio/array", 10, std::bind(&ExtractorNode::audio_callback, this, std::placeholders::_1));
-
-    // subscribe to the image topic 
-     auto image_subscriber = this->create_subscription<sensor_msgs::msg::Image>(
-            "/cae_micarray/images/pict", image_qos,
-            [this](const sensor_msgs::msg::Image::SharedPtr msg) {
-            });
-
-    image_subscription_ .subscribe(this, "/cae_micarray/images/pict");
+    image_subscription_ .subscribe(this, "/cae_micarray/images/pict",image_qos.get_rmw_qos_profile());
 
     // subscribe to the audio topic
     audio_subscription_.subscribe(this, "/cae_micarray/audio/array");
@@ -42,10 +33,15 @@ public:
     // create a publisher for the audio and image data
     av_publisher =  this->create_publisher<extractor_node::msg::AvReader>("/extractor/av_message", 10);
 
-    //create a time sychronizer for the audio and image data
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream> MySyncPolicy;
-    sync_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream>>(10);
+    // //create a time sychronizer for the audio and image data
+    // typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream> MySyncPolicy;
+    // sync_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream>>(10);
+    // sync_->connectInput(image_subscription_, audio_subscription_);
+
+    // Create a synchronizer using ApproximateTime
+    sync_ = std::make_shared<Sync>(10);
     sync_->connectInput(image_subscription_, audio_subscription_);
+
 
     // 注册同步回调函数
     sync_->registerCallback(std::bind(&ExtractorNode::av_message_callback_, this, std::placeholders::_1, std::placeholders::_2));
@@ -54,54 +50,6 @@ public:
   }
 
 private:
-
-//   // delcare the image and audio subscription
-//   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
-//   rclcpp::Subscription<cae_microphone_array::msg::AudioStream>::SharedPtr audio_subscription_;
-
-//   // delcare the image and audio publisher
-//   rclcpp::Publisher<extractor_node::msg::AvReader>::SharedPtr av_publisher;
-
-//   //delcare a vector to store the audio data
-//   std::vector<double> current_mic_data;
-
-//   void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
-//   {
-//     RCLCPP_INFO(this->get_logger(), "Image Recieved");
-
-//     // check if the audio data is available
-//     if (current_mic_data.empty())
-//     {
-//       RCLCPP_INFO(this->get_logger(), "No audio data available");
-//       return;
-//     }
-//     else
-//     {
-//       extractor_node::msg::AvReader av_reader;
-//       av_reader.header = msg->header;  
-//       av_reader.image = *msg;
-//       av_reader.audio= current_mic_data;
-//       av_publisher->publish(av_reader);
-//     }
-//   }
-
-//   void audio_callback(const cae_microphone_array::msg::AudioStream::SharedPtr msg)
-//   {
-//     RCLCPP_INFO(this->get_logger(), "Audio Recieved");
-    
-//     auto data = std::vector<uint8_t>(msg->data.begin(), msg->data.end());
-//     std::size_t length = data.size();
-//     std::size_t num_doubles = length / sizeof(double);
-
-//     // transfer the audio data to a double array
-//     std::vector<double> double_data(num_doubles);
-//     std::memcpy(double_data.data(), data.data(), length);
-//     std::cout << "Vector length: " << num_doubles << std::endl;
-
-//     current_mic_data = double_data;
-//   }
-
-// };
 
   void av_message_callback_(const sensor_msgs::msg::Image::ConstSharedPtr& image, const cae_microphone_array::msg::AudioStream::ConstSharedPtr& audio)
       {
@@ -113,9 +61,13 @@ private:
   message_filters::Subscriber<cae_microphone_array::msg::AudioStream> audio_subscription_;
 
   // create the time synchronizer
-  std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream>> sync_;
-    
   rclcpp::Publisher<extractor_node::msg::AvReader>::SharedPtr av_publisher;
+
+  // Create a synchronizer using ApproximateTime
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, cae_microphone_array::msg::AudioStream> MySyncPolicy;
+  typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+  
+  std::shared_ptr<Sync> sync_;
 };
 
 int main(int argc, char *argv[])
